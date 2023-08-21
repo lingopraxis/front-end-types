1a9849d1b10dd236fb16692cbfcf8efc5aef971b
babaruh <realmaksym@proton.me>
2023-08-21 15:17:22 +0300

    update

--- release.sh
+++ release.sh
@@ -6,12 +6,14 @@ latest_version=$(jq -r '.version' package.json)
 # Increment the patch version
 new_version=$(echo "$latest_version" | awk -F. '{print $1"."$2".$(($3+1))}')
 
+git tag $new_version
+
 # Create archives
-zip -r ../front-end-types.zip . -x ./node_modules/\* ./.git/\*
-tar -czvf ../front-end-types.tar.gz --exclude=node_modules --exclude=.git ../front-end-types
+zip -r front-end-types.zip . -x ./node_modules/\* ./.git/\*
+tar -czvf front-end-types.tar.gz --exclude=node_modules --exclude=.git ../front-end-types
 
 # Execute the command with the latest version
-gh release create "$new_version" --generate-notes '../front-end-types.tar.gz#Source.tar.gz' '../front-end-types.zip#Source.zip'
+gh release create "$new_version" --generate-notes 'front-end-types.tar.gz#Source.tar.gz' 'front-end-types.zip#Source.zip'
 
 # cleanup
-rm -rf ../front-end-types.tar.gz ../front-end-types.zip
+rm -rf front-end-types.tar.gz front-end-types.zip

e472f276cab9581ed37d3032c2516ae2f32cddfe
babaruh <realmaksym@proton.me>
2023-08-21 14:34:29 +0300

    update

--- release.sh
+++ release.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-# Retrieve the latest version from package.json
+# Retrieve the latest version from the package.json file
 latest_version=$(jq -r '.version' package.json)
 
-new_version=$(echo $latest_version | awk -F. -v OFS=. '{$NF++; print}')
-
-git tag $new_version
+# Increment the patch version
+new_version=$(echo "$latest_version" | awk -F. '{print $1"."$2".$(($3+1))}')
 
 # Create archives
-zip -r ../front-end-types.zip . -x ./node_modules/\* ./.git/\* ./generate.js
-tar -czvf ../front-end-types.tar.gz --exclude=node_modules --exclude=.git --exclude=generate.js ../front-end-types\
+zip -r ../front-end-types.zip . -x ./node_modules/\* ./.git/\*
+tar -czvf ../front-end-types.tar.gz --exclude=node_modules --exclude=.git ../front-end-types
 
 # Execute the command with the latest version
 gh release create "$new_version" --generate-notes '../front-end-types.tar.gz#Source.tar.gz' '../front-end-types.zip#Source.zip'

4f5b44a56d259299156c6586d7e191dd766baa3a
babaruh <realmaksym@proton.me>
2023-08-21 14:22:10 +0300

    update

--- release.sh
+++ release.sh
@@ -8,11 +8,11 @@ new_version=$(echo $latest_version | awk -F. -v OFS=. '{$NF++; print}')
 git tag $new_version
 
 # Create archives
-zip -r ../front-end-types-$new_version.zip . -x ./node_modules/\* ./.git/\* ./generate.js
-tar -czvf ../front-end-types-$new_version.tar.gz --exclude=node_modules --exclude=.git --exclude=generate.js ../front-end-types\
+zip -r ../front-end-types.zip . -x ./node_modules/\* ./.git/\* ./generate.js
+tar -czvf ../front-end-types.tar.gz --exclude=node_modules --exclude=.git --exclude=generate.js ../front-end-types\
 
 # Execute the command with the latest version
-gh release create "$new_version" --generate-notes '../front-end-types-$new_version.tar.gz#Source.tar.gz' '../front-end-types-$new_version.zip#Source.zip'
+gh release create "$new_version" --generate-notes '../front-end-types.tar.gz#Source.tar.gz' '../front-end-types.zip#Source.zip'
 
 # cleanup
-rm -rf ../front-end-types-$new_version.tar.gz ../front-end-types-$new_version.zip
+rm -rf ../front-end-types.tar.gz ../front-end-types.zip

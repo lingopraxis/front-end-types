aee6dcd355b0b473b57563afde7aa52822da0a8b
babaruh <realmaksym@proton.me>
2023-08-21 13:51:27 +0300

    update

--- release.sh
+++ release.sh
@@ -3,12 +3,16 @@
 # Retrieve the latest version from package.json
 latest_version=$(jq -r '.version' package.json)
 
+new_version=$(echo $latest_version | awk -F. -v OFS=. '{$NF++; print}')
+
+git tag $new_version
+
 # Create archives
-zip -r ../front-end-types-$latest_version.zip . -x ./node_modules/\* ./.git/\* ./generate.js
-tar -czvf ../front-end-types-$latest_version.tar.gz --exclude=node_modules --exclude=.git --exclude=generate.js ../front-end-types\
+zip -r ../front-end-types-$new_version.zip . -x ./node_modules/\* ./.git/\* ./generate.js
+tar -czvf ../front-end-types-$new_version.tar.gz --exclude=node_modules --exclude=.git --exclude=generate.js ../front-end-types\
 
 # Execute the command with the latest version
-gh release create "$latest_version" --generate-notes ../front-end-types-$latest_version.tar.gz ../front-end-types-$latest_version.zip
+gh release create "$new_version" --generate-notes ../front-end-types-$new_version.tar.gz ../front-end-types-$new_version.zip
 
 # cleanup
-rm -rf ../front-end-types-$latest_version.tar.gz ../front-end-types-$latest_version.zip
+rm -rf ../front-end-types-$new_version.tar.gz ../front-end-types-$new_version.zip
